e82f01204c5ad24d60437bee139df2a58d23090f
RenanGarciaTD <160497405+RenanGarciaTD@users.noreply.github.com>
2024-07-08 16:52:34 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,45 +1,34 @@
 import streamlit as st
 from openai import OpenAI
 
-# Show title and description.
-st.title("💬 Chatbot")
-
 openai_api_key = st.secrets["OPENAI_API_KEY"]
 
-# Create an OpenAI client.
-client = OpenAI(api_key=openai_api_key)
+st.set_page_config(page_title="RAG Chatbot") #HTML title
+st.title("RAG Chatbot") #page title
 
-# Create a session state variable to store the chat messages. This ensures that the
-# messages persist across reruns.
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+if 'memory' not in st.session_state: #see if the memory hasn't been created yet
+    st.session_state.memory = glib.get_memory() #initialize the memory
 
-# Display the existing chat messages via `st.chat_message`.
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+if 'chat_history' not in st.session_state:
+    st.session_state.chat_history = []
 
-# Create a chat input field to allow the user to enter a message. This will display
-# automatically at the bottom of the page.
-if prompt := st.chat_input("What is up?"):
+if 'vector_index' not in st.session_state:
+    st.session_state.vector_index = glib.get_index() 
 
-    # Store and display the current prompt.
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.markdown(prompt)
+#Re-render the chat history (Streamlit re-runs this script, so need this to preserve previous chat messages)
+for message in st.session_state.chat_history: #loop through the chat history
+    with st.chat_message(message["role"]): #renders a chat line for the given role, containing everything in the with block
+        st.markdown(message["text"]) #display the chat content
 
-    # Generate a response using the OpenAI API.
-    stream = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": m["role"], "content": m["content"]}
-            for m in st.session_state.messages
-        ],
-        stream=True,
-    )
+input_text = st.text_area("Input text", label_visibility="collapsed") #display a multiline text box with no label
+go_button = st.button("Go", type="primary") #display a primary button
+if go_button: #code in this if block will be run when the button is clicked
+    with st.chat_message("user"): #display a user chat message
+        st.markdown(input_text) #renders the user's latest message
 
-    # Stream the response to the chat using `st.write_stream`, then store it in 
-    # session state.
-    with st.chat_message("assistant"):
-        response = st.write_stream(stream)
-    st.session_state.messages.append({"role": "assistant", "content": response})
+    with st.spinner("Working..."): #show a spinner while the code in this with block runs
+        response_content = glib.get_rag_chat_response(input_text=input_text, memory=st.session_state.memory, index=st.session_state.vector_index) #call the model through the supporting library
+
+        st.write(response_content) #display the response content
+        st.session_state.chat_history.append({"role":"user", "text":input_text}) #append the user's latest message to the chat history
+        st.session_state.chat_history.append({"role":"assistant", "text":response_content}) #append the bot's latest message to the chat history
